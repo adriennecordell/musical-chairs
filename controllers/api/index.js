858116b54bb80d2796b1770ce3d5d8d2ffdfd7df
Megan Rakow <meganrakow@Megans-MacBook-Pro-2.local>
2022-12-05 20:25:28 -0600

    edited item.js in api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,12 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
+const venuesRoutes = require("../../models/venues");
+const ticketRoutes = require("../../models/tickets");
+const eventRoutes = require("../../models/events");
+const userRoutes = require("./userRoutes");
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+router.use("/tickets", ticketRoutes);
+router.use("/venues", venuesRoutes);
+router.use("/events", eventRoutes);
 
 module.exports = router;
