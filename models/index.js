9257df7597d5029d63e48449fbdb3bbdbcc9a504
adriennecordell <adrienne.cordellac@gmail.com>
2022-12-05 20:28:42 -0600

    updates index.js model file

--- models/index.js
+++ models/index.js
@@ -1,3 +1,9 @@
 const User = require('./User');
+const Tickets = require('./tickets');
+const Events = require('./events');
+const Venues = require('./venues')
 
 module.exports = { User };
+module.exports = { Tickets };
+module.exports = { Events };
+module.exports = { Venues };
