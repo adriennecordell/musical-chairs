2c1b97fce6540beec7fec2a0bc469594cdcf5697
Megan Rakow <meganrakow@Megans-MacBook-Pro-2.local>
2022-12-07 20:02:43 -0600

    venue setup

---
+++ controllers/api/venueRoutes.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const { Venues } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+  } catch {}
+});
+
+router.put("/sold_tickets/:id", async (req, res) => {
+  try {
+    //get current amount of seats form venue
+    const venue = await Venues.findByPk(req.params.id);
+
+    //num_seat - req.body
+    const totalSeats = venue.num_seats - req.body.num_tickets;
+
+    const updateVenue = await Venues.update(
+      { num_seats: totalSeats },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(updateVenue);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
