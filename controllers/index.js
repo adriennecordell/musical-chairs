8e8faf11c369f18c0d745ea8206ae57b68286e34
Brian Bulowicz <bajebi.bb@gmail.com>
2022-12-07 21:02:40 -0600

    handlebars fixed

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,12 @@
 const router = require('express').Router();
 const path = require('path')
 
-router.get('/',async (req,res)=>{//handlebars
-    res.render('index')//handlebars-render the home view content inside the main layout and send it to browser
-})
-router.get('/yurt',async(req,res)=>{//handlebars
-    res.render('yurt')//handlebars-render the home yurt content inside the main layout and send it to browser
-})
-router.get('/auditorium',async (req,res)=>{//handlebars
-    res.render('auditorium')//handlebars-render the auditorium view content inside the main layout and send it to browser
-})
-router.get('/basement',async (req,res)=>{//handlebars
-    res.render('basement')//handlebars-render the basement view content inside the main layout and send it to browser
-})
+
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const viewRoutes = require('./viewRoutes');
 
-router.use('/', homeRoutes);
+router.use('/', viewRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -29,4 +29,18 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/',async (req,res)=>{//handlebars
+  res.render('index')//handlebars-render the home view content inside the main layout and send it to browser
+})
+router.get('/yurt',async(req,res)=>{//handlebars
+  res.render('yurt')//handlebars-render the home yurt content inside the main layout and send it to browser
+})
+router.get('/auditorium',async (req,res)=>{//handlebars
+  res.render('auditorium')//handlebars-render the auditorium view content inside the main layout and send it to browser
+})
+router.get('/basement',async (req,res)=>{//handlebars
+  res.render('basement')//handlebars-render the basement view content inside the main layout and send it to browser
+})
+
+
 module.exports = router;
