6ac272268a915bbb482831bbf99451388ac6e55b
adriennecordell <adrienne.cordellac@gmail.com>
2022-12-07 19:05:36 -0600

    seeded database and linked models

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,31 @@
 const sequelize = require('../config/connection');
 const { User } = require('../models');
+const { Tickets } = require('../models');
+const { Venues } = require('../models');
+const { Events } = require('../models');
 
 const userData = require('./userData.json');
+const ticketsData = require('./ticketsData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  }); 
+
+  await Tickets.bulkCreate(ticketsData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Venues.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Events.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
