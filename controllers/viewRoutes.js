e90f842e6e9fe9b59378008229c094fad1368144
Jason <jajones414@gmail.com>
2022-12-14 17:23:34 -0600

    Added get route for signup in viewRoutes.js.

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -27,8 +27,15 @@ router.get("/login", (req, res) => {
     res.redirect("/");
     return;
   }
-
   res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+  res.render("signup");
 });
 
 router.get("/venue/:id", async (req, res) => {
